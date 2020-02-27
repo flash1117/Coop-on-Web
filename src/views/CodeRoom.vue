a467b7cbf39b32d9099234a1103b8fbb3f4374d7
김기태 <flash1117@naver.com>
2020-02-27 17:51:59 +0900

    update coderoom

--- src/views/CodeRoom.vue
+++ src/views/CodeRoom.vue
@@ -1,3 +1,85 @@
+<!--
+
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="teams"
+    sort-by="name"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>Team List</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" dark class="mb-2" v-on="on">New Team</v-btn>
+          </template>
+          
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.name" label="Team name"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.peopleNum" label="Number of People"></v-text-field>
+                  </v-col>
+                 
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.action="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        edit
+      </v-icon>
+      <v-icon
+        small
+        class="mr-2"
+        @click="deleteItem(item)"
+      >
+        delete
+      </v-icon>
+      <v-icon
+        small
+        @click="moveCodeRoom()"
+      >
+        code
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+
+ -->
+
 <template>
     <div id="app">
   <v-app light>
